00d4a55a21a714f21285d09a8be2a9e040ffd5c7
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-02-14 21:15:03 -0500

    numeros

---
+++ Lun 14 Febrero/data_types_numbers.py
@@ -0,0 +1,18 @@
+# Integers -> Numeros enteros -> int
+# Float -> Decimales -> float
+# Complejos -> Complejos, imaginarios -> complex
+# type()
+numero = 323232 
+numero_negativo = -434343
+
+print(type(numero))
+
+flotante = 43453.21
+flotante_negativo = -232.43
+
+print(type(flotante_negativo))
+
+complex_1 = complex(0, 2)
+print(type(complex_1))
+
+print(flotante)
