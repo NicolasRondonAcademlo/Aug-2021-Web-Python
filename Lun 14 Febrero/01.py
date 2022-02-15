4e11461b86fac94e335e69db19bb70ff662cf0af
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-02-14 20:44:15 -0500

    print Ex

---
+++ Lun 14 Febrero/01.py
@@ -0,0 +1,20 @@
+print(
+    "Hola Mundo"
+)
+
+print(
+    'Hola Mundo'
+)
+
+print(
+    """
+    Hola Mundo
+    """
+)
+
+print(50)
+print(10000)
+print(3.4564)
+
+print(50, 100, 3,14, "Hola Mundo")
+print(50, 100, 3,14, "Hola Mundo")
