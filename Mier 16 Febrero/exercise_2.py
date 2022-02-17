097a58c7396f58943d738d057d7e1037325b2f50
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-02-16 22:50:20 -0500

    solution

--- Mier 16 Febrero/exercise_2.py
+++ Mier 16 Febrero/exercise_2.py
@@ -1,2 +1,18 @@
 "sisetma que nos va decir si las lllaves estan balanceadas o no"
- # [[[]]] -> True  ---- [[[]] -> False --- ][ -> false
+ # [[[]]] -> True  ---- [[[]] -> False --- ][ -> false ]]]]
+
+brackets = "[][]" # Listas, Tuples, Strings, Iterados customizados
+check = 0
+for  bracket in brackets:
+    if bracket == "[":
+        check += 1
+    
+    elif bracket == "]":
+        check -= 1
+    
+    if check < 0:
+        break
+
+print(check == 0)
+
+
