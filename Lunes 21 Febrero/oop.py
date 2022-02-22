42063aff8a907d62e5640d183b7f6e464d9fb2f5
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-02-21 21:34:05 -0500

    ejercicio

--- Lunes 21 Febrero/oop.py
+++ Lunes 21 Febrero/oop.py
@@ -7,7 +7,7 @@
 
 # Empleado  -> Id, Salarym, Departmtamento.  -> Comportamientos -> Metodos,  ingresar , subir, bajar
 
-
+import urllib
 class Employee:
     ID = None
     salary = None
@@ -94,7 +94,7 @@ print ("TEam:  ", p1.team_name)
 # ejecuta algunas instrucciones y puede o no retornar un resultado
 
 class Employee:
-    def __init__(self, ID=None, salary=None, department=None):
+    def __init__(self, ID=None, salary=None, department=None ):
         self.ID = ID
         self.salary = salary
         self.department = department
@@ -106,7 +106,52 @@ class Employee:
         return  self.salary * 0.2
 
 print ("ppppppp")
+
 jacobo = Employee(12443434, salary=5400, department="Lead Tech")
 print (jacobo.salary)
 print (jacobo.salary_per_day())
 print (jacobo.tax())
+
+#  Metodos de instancia - Metodos de clase - Metodos Estaticos
+
+#Metodos de clase -> Se pueden acceder sin crear o instanciar un objeto
+
+class Myclass:
+    class_variable = "Academlo"
+
+    @classmethod
+    def demo(cls):
+        return  cls.class_variable
+
+print (Myclass.demo())
+
+class Query:
+
+    @classmethod
+    def query_tolerancia(cls, endpoint):
+        cls.VARIABLE
+        return  "DAta de tolerancia"
+
+    @classmethod
+    def query_prevencion(cls):
+        return  "Data de prevencion"
+
+
+class Employee:
+    def __init__(self, ID, salary):
+        self.ID = ID,
+        self.__salary = salary # "Privado"
+
+    def display_salary(self):
+        self.__display_id()
+        return self.__salary
+
+    def __display_id(self):
+        print ("ID: ", self.ID)
+
+Steve = Employee(3789, 2500)
+print (Steve.ID)
+print (Steve.display_salary())
+
+print (Steve._Employee__salary, "No que muy privado")
+#Steve._Employee__salary
